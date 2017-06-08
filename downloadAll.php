9f149b30a6dbbc0fa4a77a51d128d5e8fff92306
harris222 <harris222@users.noreply.github.com>
2017-06-08 13:20:00 -0700

    Add files via upload

--- downloadAll.php
+++ downloadAll.php
@@ -1,26 +1,36 @@
 <?php
 
 $pathdir = "uploads/";
-$src = $_GET["src"];
 
-$x = getenv("HOMEDRIVE") . getenv("HOMEPATH");
-$zipname = $x."\Downloads\images.zip";
-$zip = new ZipArchive;
+//$x = getenv("HOMEDRIVE") . getenv("HOMEPATH");
+$zipname = "images.zip";
+$zip = new ZipArchive();
 
 if ($zip->open($zipname, ZIPARCHIVE::OVERWRITE | ZipArchive::CREATE) == true){
+	
 	$dir = opendir($pathdir);
-	while($file = readdir($dir)){
-		for($i = 0; $i < sizeof($src); $i++){
-			if($src[$i] == $file){
-				$zip->addFile($pathdir.$file, $file);
-			} // if
-		} // for
+	while ($file = readdir($dir)){
+		$path = $pathdir.$file;
+		if($file !== "." && $file !== "..")
+		$zip->addFile($path, $file);
 	} // while
 	
 	$zip->close();
-	echo "Archive Saved in ".$x."\Downloads";
+
+    # send the file to the browser as a download
+    header('Content-disposition: attachment; filename=images.zip');
+    header('Content-type: application/zip');
+    readfile($zipname);
 	
-	// // http headers for zip downloads
+	// $path = $pathdir.$file;
+	// $dir = opendir($pathdir);
+	// while($file = readdir($dir)){
+		// $zip->addFile($pathdir.$file, $file);
+	// } // while
+	
+	// $zip->close();
+	
+	// http headers for zip downloads
 	// header("Pragma: public");
 	// header("Expires: 0");
 	// header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
@@ -29,10 +39,38 @@ if ($zip->open($zipname, ZIPARCHIVE::OVERWRITE | ZipArchive::CREATE) == true){
 	// header("Content-type: application/octet-stream");
 	// header("Content-Disposition: attachment; zipname=\"".$zipname."\"");
 	// header("Content-Transfer-Encoding: binary");
-	// ob_end_flush();
-	// @readfile($filepath.$zipname);
+	// readfile($zipname);
+	// exit;
+	
+	// $files = array('Dear GP.docx','ecommerce.doc');
+
+    // # create new zip opbject
+    // $zip = new ZipArchive();
+
+    // # create a temp file & open it
+    // $tmp_file = tempnam('.','');
+    // $zip->open($tmp_file, ZipArchive::CREATE);
+
+    // # loop through each file
+    // foreach($files as $file){
+
+        // # download file
+        // $download_file = file_get_contents($file);
+
+        // #add it to the zip
+        // $zip->addFromString(basename($file),$download_file);
+
+    // }
+
+    // # close zip
+    // $zip->close();
+
+    // # send the file to the browser as a download
+    // header('Content-disposition: attachment; filename=Resumes.zip');
+    // header('Content-type: application/zip');
+    // readfile($tmp_file);
 } else {
-	echo "Download Failed!";
+
 } // if
 
 
